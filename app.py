3b2952026c879368e2657b0e52ed9970b6610e9d
venusoncrack <109627728+venusoncrack@users.noreply.github.com>
2024-02-22 10:12:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Mi primera app!!")
+st.title("app de lindas y etereas")
 
-st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
-st.write("Facilmente puedo realizar backend y frontend.")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones aesthetics etereas lindas para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend aesthetic y frontend aesthetic.")
 image = Image.open('corazoooon.jpg')
 
 st.image(image, caption='Corazon')
